6e5913819c03983ae7554c31187bc4beb41ebcb7
André Costa Lima <andre.lima@moxy.studio>
2021-01-15 22:03:41 +0000

    chore: add polyfill deps and update polyfill globals setup

--- shims/index.js
+++ shims/index.js
@@ -1,7 +1,1 @@
 import 'react-native-polyfill-globals/auto';
-import {fetch, Headers, Request, Response} from '@react-native-community/fetch';
-
-global.Headers = Headers;
-global.Request = Request;
-global.Response = Response;
-global.fetch = fetch;
